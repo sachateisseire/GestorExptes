557e363309bb93903096fabb03cb3d42004fac22
sachateisseire <sit.teisseire@gmail.com>
2022-05-06 01:51:13 -0300

    1 test commit

--- GestorExptes/GestorExptes/views.py
+++ GestorExptes/GestorExptes/views.py
@@ -1,9 +1,6 @@
 from django.http import HttpResponse
 from django.template import Context, Template
 
-def saluda(request):
-    return HttpResponse('Hola a todos')
-
 def saluda2(self):
     miHtml = open('C:\\Users\\Oficina\\Desktop\\GestorExptes\\GestorExptes\\GestorExptes\\templates\\template.html')
     plantilla = Template(miHtml.read())
